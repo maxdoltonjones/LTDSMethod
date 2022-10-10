eddfe12062ed1e92bbb6e7408d58ef87d7c2c1f6
maxdoltonjones <110569738+maxdoltonjones@users.noreply.github.com>
2022-10-10 14:25:21 -0400

    Add files via upload

---
+++ R/Survey_effort.R
@@ -0,0 +1,35 @@
+#' Survey effort
+#'
+#' Calculates the effort as distance in meters
+#' @param points A csv file showing start and end locations for LTDS lines
+#' @param zone The UTM zone of the study site
+#' @param plot Do you want the actual lines walked to be plotted?
+#' @return Distance in meters showing effort of pilot or full survey
+#' @examples
+#' #Read in csv file showing transect lines
+#' start.end <- read_csv("./Start_end_locations.csv")
+#'
+#' #Input coordinate file
+#' effort <- samp_effort(start.end, zone = 17, plot = T)
+#' #View resulting effort
+#' effort
+#'
+#' @export
+samp_effort <- function(points, zone, plot){
+  l <- vector("list", nrow(points))
+  for(i in 1:nrow(points)){
+    l[[i]] <- Lines(list(Line(cbind(c(as.numeric(points[i, 2]), as.numeric(points[i, 4])),
+                                    c(as.numeric(points[i, 3]), as.numeric(points[i, 5]))))),
+                    as.character(i))
+  }
+  lines <- SpatialLines(l)
+  zone <- 17
+  crs(lines) <- crs(paste("+proj=utm + zone=", zone, " ellps=WGS84", sep=''))
+  effort <- gLength(lines)
+  if(plot == T){
+    plot(lines)
+  } else if(plot == F){
+
+  }
+  return(effort)
+}
