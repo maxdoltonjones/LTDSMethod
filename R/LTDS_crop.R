b3b354ebf4e667de477fa02408f8e2aa4dd1694b
maxdoltonjones <110569738+maxdoltonjones@users.noreply.github.com>
2022-10-12 10:51:12 -0400

    Add files via upload

---
+++ R/LTDS_crop.R
@@ -0,0 +1,36 @@
+#' LTDS crop
+#'
+#' Crops field data to remove observations outside of truncation distance and outliers
+#' @param ltds_data Raw field data from a full LTDS survey
+#' @param trunc The truncation distance (half distance between transects)
+#' @param save Do you want to save the data to the working directory?
+#' @return A csv file of truncated ltds data
+#' @examples
+#' #Read in csv file showing transect lines
+#' ltds_data <- read_csv("./LTDS_example_data.csv")
+#'
+#' #Input raw data into function
+#' new_ltds_data <- ltds_crop(ltds_data = ltds_data, trunc = 5, save = T)
+#'
+#' @export
+LTDS_crop <- function(ltds_data, trunc){
+
+  #Remove any distance greater than the input truncation distance (trunc)
+  ltds_data <- ltds_data[!(ltds_data$Distance > trunc),]
+  #Arrange the data in descending order and then remove the highest 5%
+  #of values
+  ltds_data <- ltds_data %>%
+    arrange(desc(Distance)) %>%
+    slice_tail(prop = 0.95)
+  #Re-arrange data by transect id
+  ltds_data <- ltds_data %>%
+    arrange(Transect_ID)
+
+  if(save == T){
+    write_csv(file = "LTDS_truncated_data.csv", x = ltds_data)
+  } else if(save == F){
+
+  }
+
+  return(ltds_data)
+}
